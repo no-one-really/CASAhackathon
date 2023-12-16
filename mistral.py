2ad58a8a0290ee22e183d0f36b3461208c77ffa0
no-one-really <58998511+no-one-really@users.noreply.github.com>
2023-12-16 13:58:24 +0530

    Update mistral.py

--- mistral.py
+++ mistral.py
@@ -1,38 +1,21 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 import sys
+import time
+start_time = time.time()
 
-statement = sys.argv[1]
-prompt2 = sys.argv[2]
-clr_st = statement.replace("#", " ")
-clr_pmt = prompt2.replace("#", " ")
+
+import intel_extension_for_pytorch as ipex
 device = "cpu"
 
-
-def find_nth_occurrence(text, target, n):
-    count = 0
-    index = -1
-
-    while count < n:
-        index = text.find(target, index + 1)
-
-        if index == -1:
-            break  # Target not found
-
-        count += 1
-
-    return index
-
-
-target_string = "[/INST]"
-nth_occurrence = 2
-
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-model = AutoModelForCausalLM.from_pretrained(
-    "mistralai/Mistral-7B-Instruct-v0.1")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+dtype = torch.float # or torch.bfloat16
+#model = ipex.optimize_transformers(model, dtype=dtype)
 messages = [
     {"role": "user", "content": "I need some assistance"},
     {"role": "assistant", "content": "I will help"},
-    {"role": "user", "content": "Will black shoe suit white pants"}
+    {"role": "user", "content": "Will black shoe suit white pants tell me "}
 
 ]
 encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
@@ -41,5 +24,6 @@ model_inputs = encodeds.to(device)
 model.to(device)
 generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
-# result = find_nth_occurrence(decoded[0], target_string, nth_occurrence)
+# result = find_nth_occurredecoded[0], target_string, nth_occurrence)
 print(decoded[0])
+print("--- %s seconds ---" % (time.time() - start_time))
